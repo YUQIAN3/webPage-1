804095d6a8d619a81c00558e547459893597f13d
yuqian <584882416@qq.com>
2022-09-07 13:51:42 +0800

    Home页面的css

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
       <h1>轱辘UI</h1>
       <h2>一个厉害的轱辘UI框架</h2>
       <p class="action">
-        <a href="">GitHub</a>
-        <a href="">Start</a>
+        <a href="http://github.com">GitHub</a>
+        <router-link to="/doc">开始</router-link>
       </p>
     </div>
   </div>
@@ -17,4 +17,26 @@ export default {
   components: { Topnav },
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .banner{
+    padding: 100px 0;
+    display: flex;
+    justify-content:center;
+    align-items: center;
+    flex-direction: column;
+    background: lightgreen;
+    > .action{
+      padding:8px 0;
+      a {
+        margin:0  8px;;
+        background: #fff;
+        display: inline-block;
+        $h: 28px;
+        height: $h;
+        line-height: $h;
+        border-radius: $h/2;
+        padding: 0 8px;
+      }
+    }
+  }
+</style>
