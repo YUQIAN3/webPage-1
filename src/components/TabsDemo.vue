af5c85a7e914e35a79775f757463011b2c5578ef
yuqian <584882416@qq.com>
2022-09-15 17:07:03 +0800

    vue 3遇到bug ，导航1切换到导航2内容可以变，再切换回来内容就不能改变了

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -2,7 +2,7 @@
   <div>
     <div> Tabs 示例</div>
     <h1>示例1</h1>
-    <Tabs>
+    <Tabs v-model:selected="x">
       <Tab title="导航1">内容1</Tab>
       <Tab title="导航2">内容2</Tab>
     </Tabs>
@@ -11,9 +11,14 @@
 <script lang="ts">
   import Tab from '../lib/Tab.vue'
   import Tabs from '../lib/Tabs.vue'
+  import {ref} from 'vue'
  export default {
   components:{
     Tabs,Tab
+  },
+  setup(){
+   const x=ref('导航1')
+   return {x}
   }
  }
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,12 +1,28 @@
 <template>
-  <div>
-    <div v-for="(t,index) in titles"  :key="index">{{t}}</div>
-    <component v-for="(c,index) in defaults" :is="c" :key="index" />
+  <div class="gulu-tabs">
+  <div class="gulu-tabs-nav">
+    <div class="gulu-tabs-nav-item" @click="select(t)"   :class="{selected:t===selected}"
+     v-for="(t,index) in titles" :key="index">{{t}}</div>
   </div>
+  <div class="gulu-tabs-content">
+    {{current}}
+    <!-- <component class="gulu-tabs-content-item"
+    :is="current" /> -->
+    <component class="gulu-tabs-content-item"
+    :is="current" />
+</div>
+</div>
 </template>
+
 <script lang="ts">
   import Tab from './Tab.vue'
+  import {computed} from 'vue'
 export default {
+  props:{
+    selected:{
+      type:String,
+    }
+  },
   setup(props,context){
   const defaults=context.slots.default()
   defaults.forEach((tag)=>{
@@ -14,11 +30,51 @@ export default {
       throw new Error('Tabs 的子标签必须是Tab')
     }
   })
+  const current=computed(()=>{
+   return defaults.filter((tag)=>{
+    return tag.props.title===props.selected
+  })[0]
+  })
   const titles=defaults.map((tag)=>{
     return tag.props.title
   })
-  return {defaults,titles}
+  const select=(title:string)=>{
+    context.emit('update:selected',title)
+  }
+  return {defaults,titles,current,select}
 }
   }
-  
 </script>
+
+<style lang="scss" scoped>
+  $blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+ .gulu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom:1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+    &-item{
+      display:none;
+    }
+    &.selected{
+      display: block;
+    }
+  }
+}
+</style>
