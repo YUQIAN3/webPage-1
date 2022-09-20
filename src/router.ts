776cd6bf962fbee8854bfdcaef0116aa217041a0
yuqian <584882416@qq.com>
2022-09-20 09:43:16 +0800

    创建路由

--- src/router.ts
+++ src/router.ts
@@ -5,15 +5,21 @@ import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
 import DocDemo from './components/DocDemo.vue'
+import Intro from './views/Intro.vue'
+import GetStart from './views/GetStart.vue'
+import Install from './views/Install.vue'
 import {createWebHashHistory,createRouter} from 'vue-router'
 const history=createWebHashHistory()
  export const router=createRouter({
   history:history,
   routes:[
-    {path:'/', component: Home} , 
+    {path:'/', component: Home} ,
     {path:'/doc', component: Doc,
         children:[
       {path:'',component:DocDemo}, 
+      {path:'intro', component: Intro}, 
+      {path:'get-start', component: GetStart}, 
+      {path:'install', component: Install}, 
       {path:'switch',component:SwitchDemo},
       {path:'button',component:ButtonDemo},
       {path:'dialog',component:DialogDemo},
