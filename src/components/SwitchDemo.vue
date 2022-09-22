125d2b20593a1628c33ad920c1fa96d2f354061e
yuqian <584882416@qq.com>
2022-09-22 11:46:12 +0800

    优化demo

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -2,50 +2,22 @@
   <!-- <div>// <itch v-model:value="bool"></Switch></div> -->
   <div>
     <h1>Switch 组件示例</h1>
-    <div class="demo">
-      <h2>常规用法</h2>
-      <div class="demo-component"></div>
-      <component :is="Switch1Demo"></component>
-      <div class="demo-act ions">
-        <Button>查看代码</Button>
-      </div>
-      <div class="demo-code">
-        <pre
-          class="language-html"
-          v-html="Prism.highlight(Switch1Demo.__sourceCode, Prism.languages.html, 'html')"
-        />
-      </div>
-    </div>
-    <div class="demo">
-      <h2>支持 disabled</h2>
-      <div class="demo-component"></div>
-      <component :is="Switch2Demo"></component>
-      <div class="demo-actions">
-        <Button>查看代码</Button>
-      </div>
-      <div class="demo-code">
-        <pre
-          class="language-html"
-          v-html="Prism.highlight(Switch2Demo.__sourceCode, Prism.languages.html, 'html')"
-        />
-      </div>
-    </div>
+
+    <Demo :component="Switch1Demo"></Demo>
+    <Demo :component="Switch2Demo"></Demo>
   </div>
 </template>
 <script lang="ts">
 import { ref } from "vue";
 import Switch from "../lib/Switch.vue";
-import Button from "../lib/Button.vue";
-import Switch1Demo from "./Switch1.Demo.vue";
+import Demo from "./Demo.vue";
 import Switch2Demo from "./Switch2.Demo.vue";
-import "prismjs";
-import "prismjs/themes/prism.css";
-const Prism = (window as any).Prism;
+import Switch1Demo from "./Switch1.Demo.vue";
 export default {
-  components: { Switch, Button },
+  components: { Switch, Switch1Demo, Switch2Demo, Demo },
   setup() {
     const bool = ref(false);
-    return { bool, Switch1Demo, Switch2Demo, Prism };
+    return { bool, Switch1Demo, Switch2Demo, Switch };
   },
 };
 </script>
